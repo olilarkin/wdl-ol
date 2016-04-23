feaece4d6e10b8c3b9814b099a96fd5936a7731f
Julijan Nikolic <julijannikolic@yahoo.com>
2016-04-23 22:44:51 +0200

    Adding better gui example project

---
+++ IPlugExamples/IPlugBetterGUIResize/IPlugBetterGUIResize.cpp
@@ -0,0 +1,99 @@
+#include "IPlugBetterGUIResize.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControl.h"
+#include "resource.h"
+
+const int kNumPrograms = 2;
+
+enum EParams
+{
+  kGain = 0,
+  kGain1 = 0,
+  kNumParams
+};
+
+IPlugBetterGUIResize::IPlugBetterGUIResize(IPlugInstanceInfo instanceInfo)
+  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mGain(1.)
+{
+  TRACE;
+
+  pGraphics = MakeGraphics(this, GUI_WIDTH, GUI_HEIGHT);
+  
+  // Your custom controls------------------------------------------------------------------------------------------------------
+  pGraphics->AttachBackground(BACKGROUND_ID, BACKGROUND_FN);
+  
+  //arguments are: name, defaultVal, minVal, maxVal, step, label
+  GetParam(kGain)->InitDouble("Gain", 50., 0., 100.0, 0.01, "%");
+  GetParam(kGain)->SetShape(2.);
+  IBitmap *knob = pGraphics->LoadPointerToBitmap(KNOB_ID, KNOB_FN, 60);
+  pGraphics->AttachControl(new IKnobMultiControl(this, 50, 50, kGain, knob));
+
+  //arguments are: name, defaultVal, minVal, maxVal, step, label
+  GetParam(kGain1)->InitDouble("Gain1", 50., 0., 100.0, 0.01, "%");
+  GetParam(kGain1)->SetShape(2.);
+  IBitmap *knob1 = pGraphics->LoadPointerToBitmap(KNOB1_ID, KNOB1_FN, 60, true);
+  pGraphics->AttachControl(new IKnobMultiControl(this, 600, 200, kGain1, knob1));
+
+  IRECT tmpRect3(20, 760, 800, 800);
+  IText textProps3(24, &COLOR_WHITE, "Arial", IText::kStyleItalic, IText::kAlignNear, 0, IText::kQualityDefault);
+  pGraphics->AttachControl(new ITextControl(this, tmpRect3, &textProps3, "This is IPlugGUIResize example by Youlean..."));
+
+  // GUI resize control must be the last one ----------------------------------------------------------------------------------
+  pGraphics->AttachControl(pGUIResize = new IPlugGUIResize(this, pGraphics, GUI_WIDTH, GUI_HEIGHT, BUNDLE_NAME, 16, 16));
+  // --------------------------------------------------------------------------------------------------------------------------
+
+  AttachGraphics(pGraphics);
+
+  pGraphics->ShowControlBounds(true);
+
+  //MakePreset("preset 1", ... );
+  MakeDefaultPreset((char *) "-", kNumPrograms);
+}
+
+IPlugBetterGUIResize::~IPlugBetterGUIResize() {}
+
+void IPlugBetterGUIResize::OnGUIOpen()
+{ 
+	TRACE; 
+	pGUIResize->ResizeAtGUIOpen();
+
+}
+
+void IPlugBetterGUIResize::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
+{
+  // Mutex is already locked for us.
+
+  double* in1 = inputs[0];
+  double* in2 = inputs[1];
+  double* out1 = outputs[0];
+  double* out2 = outputs[1];
+
+  for (int s = 0; s < nFrames; ++s, ++in1, ++in2, ++out1, ++out2)
+  {
+   // *out1 = *in1 * mGain;
+   // *out2 = *in2 * mGain;
+  }
+}
+
+void IPlugBetterGUIResize::Reset()
+{
+  TRACE;
+  IMutexLock lock(this);
+}
+
+void IPlugBetterGUIResize::OnParamChange(int paramIdx)
+{
+  IMutexLock lock(this);
+
+  switch (paramIdx)
+  {
+    case kGain:
+      mGain = GetParam(kGain)->Value() / 100.;
+      break;
+
+    default:
+      break;
+  }
+}
+
+
---
+++ IPlugExamples/IPlugBetterGUIResize/IPlugBetterGUIResize.h
@@ -0,0 +1,24 @@
+#ifndef __IPLUGBETTERGUIRESIZE__
+#define __IPLUGBETTERGUIRESIZE__
+
+#include "IPlug_include_in_plug_hdr.h"
+#include "IPlugGUIResize.h"
+
+class IPlugBetterGUIResize : public IPlug
+{
+public:
+  IPlugBetterGUIResize(IPlugInstanceInfo instanceInfo);
+  ~IPlugBetterGUIResize();
+
+  void Reset();
+  void OnParamChange(int paramIdx);
+  void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+  void OnGUIOpen();
+
+private:
+  double mGain;
+  IGraphics* pGraphics;
+  IPlugGUIResize* pGUIResize;
+};
+
+#endif
