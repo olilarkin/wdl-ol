9570e8d6f410bbf549a01ce16ba0999d2120b83f
Oli Larkin <olilarkin@googlemail.com>
2017-10-06 17:02:54 +0100

    Revert "remove 10.6 case in getstate. THIS NEEDS LOOKING AT!"
    This reverts commit fddec6a544b130cae6982a38a88371eaa16ce663.

--- WDL/IPlug/IPlugAU.cpp
+++ WDL/IPlug/IPlugAU.cpp
@@ -1386,6 +1386,9 @@ OSStatus IPlugAU::GetState(CFPropertyListRef* ppPropList)
   AudioComponentDescription cd;
   AudioComponent comp = AudioComponentInstanceGetComponent(mCI);
   OSStatus r = AudioComponentGetDescription(comp, &cd);
+#elif MAC_OS_X_VERSION_MAX_ALLOWED == 1060
+  ComponentDescription cd;
+  OSStatus r = GetComponentInfo((Component) mCI, &cd, 0, 0, 0);
 #else
   AudioComponentDescription cd;
   OSStatus r = GetComponentInfo((Component) mCI, &cd, 0, 0, 0);
@@ -1422,6 +1425,10 @@ OSStatus IPlugAU::SetState(CFPropertyListRef pPropList)
   AudioComponentDescription cd;
   AudioComponent comp = AudioComponentInstanceGetComponent(mCI);
   OSStatus r = AudioComponentGetDescription(comp, &cd);
+#elif MAC_OS_X_VERSION_MAX_ALLOWED == 1060
+  ComponentDescription cd;
+  OSStatus r = GetComponentInfo((Component) mCI, &cd, 0, 0, 0);
+#else
   AudioComponentDescription cd;
   OSStatus r = GetComponentInfo((Component) mCI, &cd, 0, 0, 0);
 #endif
