8381fa81fb2bcd9852295d08ba5743928f9db463
Oli Larkin <olilarkin@googlemail.com>
2018-01-30 20:18:41 +0000

    logging of view controller

--- IPlug/AUv3/IPlugViewController.mm
+++ IPlug/AUv3/IPlugViewController.mm
@@ -1,5 +1,6 @@
 #import <CoreAudioKit/AUViewController.h>
 #import "IPlugAUAudioUnit.h"
+#include "IPlugLogger.h"
 
 @interface IPlugViewController : AUViewController <AUAudioUnitFactory>
 @property (nonatomic, retain) AUAudioUnit* audioUnit; // TODO: what is @property (nonatomic)
@@ -10,23 +11,30 @@
 
 - (AUAudioUnit*) createAudioUnitWithComponentDescription:(AudioComponentDescription) desc error:(NSError **)error
 {
+  TRACE;
   self.audioUnit = [[[IPlugAUAudioUnit alloc] initWithComponentDescription:desc error:error] retain];
   
   return self.audioUnit;
 }
 
-- (void) viewDidLoad
-{
-  [super viewDidLoad];
-}
+//- (void) viewDidLoad
+//{
+//  TRACE;
+//
+//  [super viewDidLoad];
+//}
 
 - (AUAudioUnit *)getAudioUnit
 {
+  TRACE;
+
   return _audioUnit;
 }
 
 - (void)setAudioUnit:(AUAudioUnit*) audioUnit
 {
+  TRACE;
+
   _audioUnit = audioUnit;
   
   dispatch_async(dispatch_get_main_queue(), ^
@@ -82,6 +90,8 @@
 
 - (void)setFrame:(CGRect)newSize
 {
+  TRACE;
+
   [super.view setFrame:newSize];
 //  ViewRect viewRect (0, 0, newSize.size.width, newSize.size.height);
 //  
@@ -92,11 +102,13 @@
 //#define PLATFORM_VIEW UIView
 #define PLATFORM_VIEW NSView
 
-- (void)loadView
-{
-  PLATFORM_VIEW* view = [[PLATFORM_VIEW alloc] initWithFrame:CGRectMake (0, 0, 0, 0)];
-  [self setView:view];
-}
+//- (void)loadView
+//{
+//  TRACE;
+//
+//  PLATFORM_VIEW* view = [[PLATFORM_VIEW alloc] initWithFrame:CGRectMake (0, 0, 0, 0)];
+//  [self setView:view];
+//}
 
 - (instancetype) initWithNibName:(nullable NSString*) nib bundle: (nullable NSBundle*) bndl
 {
@@ -106,15 +118,21 @@
 
 - (CGSize) preferredContentSize
 {
+  TRACE;
+
   return CGSizeMake (300, 300);
 }
 
 - (void)viewDidLayoutSubviews
 {
+  TRACE;
+
 }
 
 - (NSIndexSet *)supportedViewConfigurations:(NSArray<AUAudioUnitViewConfiguration *> *)availableViewConfigurations
 {
+  TRACE;
+
   NSIndexSet* pSet = [[NSMutableIndexSet alloc] init];
 
   return pSet;
@@ -122,6 +140,8 @@
 
 - (void)selectViewConfiguration:(AUAudioUnitViewConfiguration *)viewConfiguration
 {
+  TRACE;
+
 }
 
 
